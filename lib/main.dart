c2db65b265dc7eb3f8dc412903b1c7d08c02a13d
Mark Philipp <mphilipp622@gmail.com>
2019-02-23 15:06:22 -0800

    Flutter Project Added

---
+++ lib/main.dart
@@ -0,0 +1,384 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobile_vision/flutter_mobile_vision.dart';
+import 'package:flutter_mobile_vision_example/barcode_detail.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int _cameraBarcode = FlutterMobileVision.CAMERA_BACK;
+  int _onlyFormatBarcode = Barcode.ALL_FORMATS;
+  bool _autoFocusBarcode = true;
+  bool _torchBarcode = false;
+  bool _multipleBarcode = false;
+  bool _waitTapBarcode = false;
+  bool _showTextBarcode = false;
+  Size _previewBarcode;
+  List<Barcode> _barcodes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterMobileVision.start().then((previewSizes) => setState(() {
+      _previewBarcode = previewSizes[_cameraBarcode].first;
+    }));
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  List<DropdownMenuItem<int>> _getCameras() {
+    List<DropdownMenuItem<int>> cameraItems = [];
+
+    cameraItems.add(new DropdownMenuItem(
+      child: new Text('BACK'),
+      value: FlutterMobileVision.CAMERA_BACK,
+    ));
+
+    cameraItems.add(new DropdownMenuItem(
+      child: new Text('FRONT'),
+      value: FlutterMobileVision.CAMERA_FRONT,
+    ));
+
+    return cameraItems;
+  }
+
+  List<DropdownMenuItem<Size>> _getPreviewSizes(int facing) {
+    List<DropdownMenuItem<Size>> previewItems = [];
+
+    List<Size> sizes = FlutterMobileVision.getPreviewSizes(facing);
+
+    if (sizes != null) {
+      sizes.forEach((size) {
+        previewItems.add(
+          new DropdownMenuItem(
+            child: new Text(size.toString()),
+            value: size,
+          ),
+        );
+      });
+    } else {
+      previewItems.add(
+        new DropdownMenuItem(
+          child: new Text('Empty'),
+          value: null,
+        ),
+      );
+    }
+
+    return previewItems;
+  }
+
+  List<DropdownMenuItem<int>> _getFormats() {
+    List<DropdownMenuItem<int>> formatItems = [];
+
+    Barcode.mapFormat.forEach((key, value) {
+      formatItems.add(
+        new DropdownMenuItem(
+          child: new Text(value),
+          value: key,
+        ),
+      );
+    });
+
+    return formatItems;
+  }
+
+  Widget _getBarcodeScreen(BuildContext context) {
+    List<Widget> items = [];
+
+    items.add(new Padding(
+      padding: const EdgeInsets.only(
+        top: 8.0,
+        left: 18.0,
+        right: 18.0,
+      ),
+      child: const Text('Camera:'),
+    ));
+
+    items.add(new Padding(
+      padding: const EdgeInsets.only(
+        left: 18.0,
+        right: 18.0,
+      ),
+      child: new DropdownButton(
+        items: _getCameras(),
+        onChanged: (value) {
+          _previewBarcode = null;
+          setState(() => _cameraBarcode = value);
+        },
+        value: _cameraBarcode,
+      ),
+    ));
+
+    items.add(new Padding(
+      padding: const EdgeInsets.only(
+        top: 8.0,
+        left: 18.0,
+        right: 18.0,
+      ),
+      child: const Text('Preview size:'),
+    ));
+
+    items.add(new Padding(
+      padding: const EdgeInsets.only(
+        left: 18.0,
+        right: 18.0,
+      ),
+      child: new DropdownButton(
+        items: _getPreviewSizes(_cameraBarcode),
+        onChanged: (value) {
+          setState(() => _previewBarcode = value);
+        },
+        value: _previewBarcode,
+      ),
+    ));
+
+    items.add(new Padding(
+      padding: const EdgeInsets.only(
+        top: 8.0,
+        left: 18.0,
+        right: 18.0,
+      ),
+      child: const Text('Scan format only:'),
+    ));
+
+    items.add(new Padding(
+      padding: const EdgeInsets.only(
+        left: 18.0,
+        right: 18.0,
+      ),
+      child: new DropdownButton(
+        items: _getFormats(),
+        onChanged: (value) => setState(
+              () => _onlyFormatBarcode = value,
+        ),
+        value: _onlyFormatBarcode,
+      ),
+    ));
+
+    items.add(new SwitchListTile(
+      title: const Text('Auto focus:'),
+      value: _autoFocusBarcode,
+      onChanged: (value) => setState(() => _autoFocusBarcode = value),
+    ));
+
+    items.add(new SwitchListTile(
+      title: const Text('Torch:'),
+      value: _torchBarcode,
+      onChanged: (value) => setState(() => _torchBarcode = value),
+    ));
+
+    items.add(new SwitchListTile(
+      title: const Text('Multiple Scan:'),
+      value: _multipleBarcode,
+      onChanged: (value) => setState(() {
+        _multipleBarcode = value;
+        if (value) _waitTapBarcode = true;
+      }),
+    ));
+
+    items.add(new SwitchListTile(
+      title: const Text('Wait a tap to capture:'),
+      value: _waitTapBarcode,
+      onChanged: (value) => setState(() {
+        _waitTapBarcode = value;
+        if (!value) _multipleBarcode = false;
+      }),
+    ));
+
+    items.add(new SwitchListTile(
+      title: const Text('Show text:'),
+      value: _showTextBarcode,
+      onChanged: (value) => setState(() => _showTextBarcode = value),
+    ));
+
+    items.add(
+      new Padding(
+        padding: const EdgeInsets.only(
+          left: 18.0,
+          right: 18.0,
+          bottom: 12.0,
+        ),
+        child: new RaisedButton(
+          onPressed: _scan,
+          child: new Text('SCAN!'),
+        ),
+      ),
+    );
+
+    items.addAll(
+      ListTile.divideTiles(
+        context: context,
+        tiles: _barcodes
+            .map(
+              (barcode) => new BarcodeWidget(barcode),
+        )
+            .toList(),
+      ),
+    );
+
+    return new ListView(
+      padding: const EdgeInsets.only(
+        top: 12.0,
+      ),
+      children: items,
+    );
+  }
+
+  Future<Null> _scan() async {
+    List<Barcode> barcodes = [];
+    try {
+      barcodes = await FlutterMobileVision.scan(
+        flash: _torchBarcode,
+        autoFocus: _autoFocusBarcode,
+        formats: _onlyFormatBarcode,
+        multiple: _multipleBarcode,
+        waitTap: _waitTapBarcode,
+        showText: _showTextBarcode,
+        preview: _previewBarcode,
+        camera: _cameraBarcode,
+        fps: 15.0,
+      );
+    } on Exception {
+      barcodes.add(new Barcode('Failed to get barcode.'));
+    }
+
+    if (!mounted) return;
+
+    setState(() => _barcodes = barcodes);
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+///
+/// BarcodeWidget
+///
+class BarcodeWidget extends StatelessWidget {
+  final Barcode barcode;
+
+  BarcodeWidget(this.barcode);
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      leading: const Icon(Icons.star),
+      title: new Text(barcode.displayValue),
+      subtitle: new Text(
+        '${barcode.getFormatString()} (${barcode.format}) - '
+            '${barcode.getValueFormatString()} (${barcode.valueFormat})',
+      ),
+      trailing: const Icon(Icons.arrow_forward),
+      onTap: () => Navigator.of(context).push(
+        new MaterialPageRoute(
+          builder: (context) => new BarcodeDetail(barcode),
+        ),
+      ),
+    );
+  }
+}
